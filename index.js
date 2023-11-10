1507cfce903e74ac1af9447f809e6ff4222fe294
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-11-10 20:15:18 -0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('./defaults.js');
 async function startSystem() {
     let update = await require('./update.js')();
-    console.log(update);
     if (update) {
         return;
     } else {
         try {
-            require('./system2.js')();
+            require('./system.js');
         } catch(e) {
+            console.log(e);
             if (e.code === 'MODULE_NOT_FOUND') {
                 let exec = require('child_process').exec;
                 exec(`cd ${__dirname} && npm install`, (err, stdout, stderr) => {
