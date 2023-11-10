ae056f216afe7b65852b314b64c2dd7fea130b60
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-11-10 20:12:48 -0300

    update

--- update.js
+++ update.js
@@ -1,27 +1,29 @@
 let exec = require('child_process').exec;
 module.exports = async function () {
-    console.log('checking for updates');
-    await exec(`cd ${__dirname} && git fetch && git status`, async (err, stdout, stderr) => {
-        if (stdout.includes('Your branch is up to date with \'origin/main\'.')) {
-            console.log('up to date');
-            return false;
-        } else {
-            console.log('new version found');
-            await exec(`cd ${__dirname} && git reset --hard && git pull`, async (err, stdout, stderr) => {
-                console.log(stdout);
-                if (stdout.includes('Already up to date.')) {
-                    console.log('up to date');
-                    return false;
-                } else {
-                    console.log('new version found');
-                    await exec(`cd ${__dirname} && npm install`, async (err, stdout, stderr) => {
-                        console.log(stdout);
-                        console.log('restarting');
-                        await process.exit();
-                        return true;
-                    });
-                }
-            });
-        }
+    return new Promise((resolve, reject) => {
+        console.log('checking for updates');
+        exec(`cd ${__dirname} && git fetch && git status`, (err, stdout, stderr) => {
+            if (stdout.includes('Your branch is up to date with \'origin/main\'.')) {
+                console.log('up to date');
+                resolve(false);
+            } else {
+                console.log('new version found');
+                exec(`cd ${__dirname} && git reset --hard && git pull`, (err, stdout, stderr) => {
+                    console.log(stdout);
+                    if (stdout.includes('Already up to date.')) {
+                        console.log('up to date');
+                        resolve(false);
+                    } else {
+                        console.log('new version found');
+                        exec(`cd ${__dirname} && npm install`, (err, stdout, stderr) => {
+                            console.log(stdout);
+                            console.log('restarting');
+                            process.exit();
+                            resolve(true);
+                        });
+                    }
+                });
+            }
+        });
     });
 }
