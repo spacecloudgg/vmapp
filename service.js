8e174ca0d6a615b023948c09542302f84301777d
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-12-19 02:57:10 -0300

    update

--- service.js
+++ service.js
@@ -4,7 +4,7 @@ const Service = require('node-windows').Service;
 const svc = new Service({
     name: 'SpaceCloud System',
     description: 'SpaceCloud System',
-    script: 'C:\\Users\\GAME-SYS\\AppData\\Local\\SpaceCloud\\vmapp\\resurrect.js',
+    script: path.join(__dirname, 'resurrect.js')
 });
 svc.on('install', () => {
     svc.start();
