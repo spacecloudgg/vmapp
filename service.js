4a05524196b049bde494e63a89d1ed201c0e0a8f
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-12-19 02:38:01 -0300

    update

--- service.js
+++ service.js
@@ -1,9 +1,10 @@
 const Service = require('node-windows').Service;
+//C:\Users\GAME-SYS\AppData\Local\SpaceCloud\vmapp\index.js
 
 const svc = new Service({
     name: 'SpaceCloud System',
     description: 'SpaceCloud System',
-    script: `C:\\Users\\GAME-SYS\\AppData\\Local\\SpaceCloud\\vmapp\\index.js`
+    script: path.join(__dirname, 'index.js'),
   });
   svc.on('install', () => {
     svc.start();
