98d84946e391aae8e51c56c247fca0ffb62bb329
roni0028 <76240520+roni0028@users.noreply.github.com>
2023-12-19 02:48:01 -0300

    update

--- service.js
+++ service.js
@@ -5,8 +5,16 @@ const svc = new Service({
     name: 'SpaceCloud System',
     description: 'SpaceCloud System',
     script: 'C:\\Users\\GAME-SYS\\AppData\\Local\\SpaceCloud\\vmapp\\resurrect.js',
-  });
-  svc.on('install', () => {
+});
+/*svc.on('install', () => {
     svc.start();
-  });
-  svc.install();
+});
+svc.install();*/
+
+//uninstall
+svc.on('uninstall', () => {
+    console.log('Uninstall complete.');
+    console.log('The service exists: ', svc.exists);
+});
+
+svc.uninstall();
